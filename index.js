10b30727e3f7517e8c86be47bd003a155c8cd795
vince s <vinceshury@gmail.com>
2020-11-07 11:19:37 -0500

    updated commands, cleaned up command list

--- index.js
+++ index.js
@@ -10,34 +10,36 @@ const { prefix } = require('./config.json')
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 // create a new Discord client
 const client = new Discord.Client()
-
+const dogImageApiURL = 'https://dog.ceo/api/breed'
+const dadJokeApiURL = 'https://icanhazdadjoke.com/'
+const dadJokeApiConfig = {
+  headers: {
+    Accept: 'text/plain',
+    'User-Agent': 'DiscBot - vinceshury@gmail.com'
+  }
+}
 // Response for empty prefixed messages.
-const botIntroMessage = `
-**Hello! I'm DiscBot**
-I will respond to messages that are prefixed by \`>\`
-*To list all currently available commands* **type** \`>help\`
+const botIntroMessage = `**Hello! I'm DiscBot**
+I will respond to messages that are prefixed by \`>command-name\`
+
+Example
+\`>help\` - Lists all available commands
 `
 // Command list of all available commands.
-const helpCommandList = `
-**DiscBot Command List**
+const helpCommandList = `**DiscBot Command List**
 
-\`>help\` - Lists all available commands
-
-\`>ping\` - Responds \`Pong\`
-\`>beep\` - Responds \`Boop\`
-\`>server\` - Displays server information
+\`>server\` - Displays current server information
 \`>user-info\` - Displays username and ID
-\`>joke\` - Responds with a random joke
+\`>joke\` - Tells a random joke
+\`>dog\` - Shows a random Dog image
 \`>avatar\` - Displays your avatar
 
-*No Work In Progress commands currently*
 `
+// *Works In Progress*
+
 // Simple dad joke API
-const getDadJoke = () => {
-  return axios.get('https://icanhazdadjoke.com/', {
-    headers: { 'Accept': 'text/plain', 'User-Agent':'DiscBot - vinceshury@gmail.com'}
-  })
-}
+const getDadJoke = () => axios.get(dadJokeApiURL, dadJokeApiConfig)
+const getRandomDogImage = () => axios.get(`${dogImageApiURL}s/image/random`)
 
 // Connect to the Discord
 client.on('ready', () => {
@@ -45,16 +47,17 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
+  console.log(
+    `#${message.channel.name}-@${message.author.username}#${message.author.discriminator}`
+  )
   console.log(message.cleanContent)
+  console.log('-'.repeat(20))
 
   const newMessage = message.content.toLowerCase()
 
   switch (newMessage) {
-    case `${prefix}ping`:
-      message.channel.send('Pong.')
-      break
-    case `${prefix}beep`:
-      message.channel.send('Boop.')
+    case `${prefix}help`:
+      message.channel.send(helpCommandList)
       break
     case `${prefix}server`:
       message.channel.send(
@@ -66,16 +69,20 @@ client.on('message', message => {
         `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
       )
       break
-    case `${prefix}help`:
-      message.channel.send(helpCommandList)
-      break
     case `${prefix}joke`:
       getDadJoke()
         .then(res => message.channel.send(res.data))
         .catch(apiError => console.log(apiError))
       break
     case `${prefix}avatar`:
-			message.channel.send(message.author.displayAvatarURL());
+      message.channel.send(message.author.displayAvatarURL())
+      break
+    case `${prefix}dog`:
+      getRandomDogImage()
+        .then(dogRes => {
+          message.channel.send(dogRes.data.message)
+        })
+        .catch(dogApiError => console.log(dogApiError))
       break
     case prefix:
       message.channel.send(botIntroMessage)
@@ -97,3 +104,9 @@ client.on('guildMemberAdd', member => {
 
 // login to Discord with your app's token
 client.login(DISCORD_TOKEN)
+
+
+
+// TODOS
+// https://owlbot.info/?q=parrot
+// Animal/dictionary API to solve Alex's >parakeet nonsense. 
