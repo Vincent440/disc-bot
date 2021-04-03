e5dc9611f89fb84037f177733ef6b452fc8a661c
Vincent440 <vinceshury@gmail.com>
2021-04-03 02:46:22 -0400

    Created MTG command

---
+++ commands/fun/mtg.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+  name: "mtg",
+  description: "\`>mtg\` - Searches for a Magic The Gathering Card",
+  execute(message, args) {
+    // Gonna get some card stuff here
+
+
+    message.channel.send("WIP");
+  },
+};
