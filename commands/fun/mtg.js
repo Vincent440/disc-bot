93616daaaf7fa14c71359d35244b5910ac4c4c32
Vincent440 <vinceshury@gmail.com>
2021-04-06 23:29:40 -0400

    Updated help command to display as code block for usage, mtg replies with first card image found

--- commands/fun/mtg.js
+++ commands/fun/mtg.js
@@ -1,12 +1,26 @@
+const mtg = require('mtgsdk')
 
 
 module.exports = {
   name: "mtg",
   description: "\`>mtg\` - Searches for a Magic The Gathering Card",
+  args: true,
+  cooldown: 6,
   execute(message, args) {
-    // Gonna get some card stuff here
+    const query = args.join(' ').trim()
+    console.log(query)
+    if (query) {
+      return mtg.card.where({ name: query, pageSize: 1 })
+        .then(cardList => {
+          console.log(cardList[0])
+          console.log(cardList[0].name)
+          message.channel.send(cardList[0].imageUrl)
+        })
 
+    }
 
-    message.channel.send("WIP");
+    return message.channel.send("No cards found, please try a different search.");
   },
+  aliases: ["commands"],
+  usage: "<card name>",
 };
