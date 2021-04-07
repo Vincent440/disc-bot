6c79e4050d7be4553d39db6cfcab8fbb6744732d
Vincent440 <vinceshury@gmail.com>
2021-04-07 00:15:14 -0400

    Removed events from index.js to event files

---
+++ events/guild-member-add.js
@@ -0,0 +1,13 @@
+module.exports = {
+      name: 'guildMemberAdd',
+      execute(member) {
+            // Send the message to a designated channel on a server:
+            const channel = member.guild.channels.cache.find((channelToNotify) => channelToNotify.name === "general");
+            console.log(member)
+            console.log(channel)
+            // Do nothing if the channel wasn't found on this server
+            if (!channel) return;
+            // Send the message, mentioning the member
+            channel.send(`Welcome to ${member.guild.name}, ${member}`);
+      },
+};
--- index.js
+++ index.js
@@ -3,22 +3,20 @@ require("dotenv").config();
 
 const fs = require("fs");
 const Discord = require("discord.js");
-const { prefix } = require("./config.json");
+
 const commandFolders = fs.readdirSync("./commands");
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 const client = new Discord.Client();
 const cooldowns = new Discord.Collection();
 
-// Create a new command collection (similar to JavaScript Maps)
 client.commands = new Discord.Collection();
-
 
 
 for (const folder of commandFolders) {
   const commandFiles = fs.readdirSync(`./commands/${folder}`).filter((file) => file.endsWith(".js"));
-
   for (const file of commandFiles) {
     // Gets all files from the commands folder.
     const command = require(`./commands/${folder}/${file}`);
@@ -29,99 +27,15 @@ for (const folder of commandFolders) {
   }
 }
 
-/**
- * The message the BOT has access to respond to
- * @param {?string} message
- */
-const handleMessage = (message) => {
-  // Don't reply to bots or do anything without the command prefix
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+for (const eventFile of eventFiles) {
+	const event = require(`./events/${eventFile}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-  /**
-   * Array of strings containing the *optional* `args`
-   *
-   * **Does not** include the `prefix` or `command`
-   * @constant
-   * @type {string[]}
-   */
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-
-  /**
-   * The name of the command the user inputs after the BOT prefix
-   * @constant
-   * @type {string}
-   */
-  const commandName = args.shift().toLowerCase();
-
-  // If there isn't a command with that name, exit early.
-
-  if (!client.commands.has(commandName)) return;
-
-  const command =
-    client.commands.get(commandName) || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
-
-  if (command.serverOnly && message.channel.type === "dm") {
-    return message.reply("I can't execute that command inside a Direct Message!");
-  }
-
-  // If args is set to true in a command file, return
-  if (command.args && !args.length) {
-    let reply = `You didn't provide any arguments, ${message.author}!`;
-
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-    }
-
-    return message.channel.send(reply);
-  }
-
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Discord.Collection());
-  }
-
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  const cooldownAmount = (command.cooldown || 3) * 1000;
-
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(
-        `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
-      );
-    }
-  }
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply("There was an error trying to execute that command!");
-  }
-};
-
-// Connect to the Discord
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("message", handleMessage);
-
-// Create an event listener for new guild members
-client.on("guildMemberAdd", (member) => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find((channelToNotify) => channelToNotify.name === "general");
-  console.log(member)
-  console.log(channel)
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to ${member.guild.name}, ${member}`);
-});
 
 // login to Discord with your app's token
 client.login(DISCORD_TOKEN);
