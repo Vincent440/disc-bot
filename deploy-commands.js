ebc07c76ddc009e0a5fe615f8933f63f6d2f0ac9
Vincent Shury <vinceshury@gmail.com>
2023-08-18 23:53:47 -0400

    Updated deploy-commands.js to pull from commands/

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,26 @@
 'use strict'
 require('dotenv').config()
 
-const { SlashCommandBuilder } = require('@discordjs/builders')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
+const fs = require('node:fs')
+const path = require('node:path')
 
 const { DISCORD_TOKEN, CLIENT_ID } = process.env
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong!'),
-  new SlashCommandBuilder()
-    .setName('server')
-    .setDescription('Replies with server info!'),
-  new SlashCommandBuilder()
-    .setName('user')
-    .setDescription('Replies with user info!')
-].map(command => command.toJSON())
-
 const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN)
+
+const commands = []
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file)
+  const command = require(filePath)
+  commands.push(command.data.toJSON())
+}
 
 ;(async () => {
   try {
