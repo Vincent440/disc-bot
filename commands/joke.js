0dd4eb0136c7113d852ee015e66523c3b04f7cb5
Vincent440 <vinceshury@gmail.com>
2024-01-11 15:02:46 -0500

    Updated joke.js - replaced axios with fetch

--- commands/joke.js
+++ commands/joke.js
@@ -1,15 +1,21 @@
-const axios = require('axios')
+// const axios = require('axios')
 const { SlashCommandBuilder } = require('discord.js')
 
-const getRandomDadJoke = () =>
-  axios({
-    method: 'get',
-    url: 'https://icanhazdadjoke.com/',
-    headers: {
-      Accept: 'text/plain',
-      'User-Agent': 'DiscBot - vinceshury@gmail.com'
-    }
-  })
+const API_URL = 'https://icanhazdadjoke.com/'
+
+const requestOptions = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'User-Agent': 'DiscBot - vinceshury@gmail.com'
+  }
+}
+
+const getRandomDadJoke = () => {
+  return fetch(API_URL, requestOptions)
+    .then(response => response.json())
+    .then(responseData => responseData)
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -17,7 +23,7 @@ module.exports = {
     .setDescription('Tells a random joke, from the icanhazdadjoke API.'),
   async execute (interaction) {
     await getRandomDadJoke()
-      .then(res => interaction.reply(res.data))
+      .then(data => interaction.reply(data.joke))
       .catch(apiError => {
         console.log(apiError)
         return interaction.reply(
