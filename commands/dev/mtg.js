0daecb63e0ab4cb029deb8c0c5362796ec6ef3a9
Vincent440 <vinceshury@gmail.com>
2024-01-11 13:51:21 -0500

    commented out mtg.js needs replaced with a better alternative and updated to new syntax

--- commands/dev/mtg.js
+++ commands/dev/mtg.js
@@ -1,29 +1,29 @@
-const mtg = require('mtgsdk')
+// const mtg = require('mtgsdk')
 
-module.exports = {
-  name: 'mtg',
-  description: '`>mtg` - Searches for a Magic The Gathering Card',
-  args: true,
-  cooldown: 5,
-  execute (message, args) {
-    const query = args.join(' ').trim()
-    console.log(query)
-    if (query) {
-      return mtg.card
-        .where({ name: query, pageSize: 1 })
-        .then(cardList => {
-          message.channel.send(
-            cardList[0]?.imageUrl ?? 'No cards found, sorry try again!'
-          )
-        })
-        .catch(mtgError => {
-          console.log(mtgError)
-        })
-    }
+// module.exports = {
+//   name: 'mtg',
+//   description: '`>mtg` - Searches for a Magic The Gathering Card',
+//   args: true,
+//   cooldown: 5,
+//   execute (message, args) {
+//     const query = args.join(' ').trim()
+//     console.log(query)
+//     if (query) {
+//       return mtg.card
+//         .where({ name: query, pageSize: 1 })
+//         .then(cardList => {
+//           message.channel.send(
+//             cardList[0]?.imageUrl ?? 'No cards found, sorry try again!'
+//           )
+//         })
+//         .catch(mtgError => {
+//           console.log(mtgError)
+//         })
+//     }
 
-    return message.channel.send(
-      'No cards found, please try a different search.'
-    )
-  },
-  usage: '[card name]'
-}
+//     return message.channel.send(
+//       'No cards found, please try a different search.'
+//     )
+//   },
+//   usage: '[card name]'
+// }
